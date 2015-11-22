5a8d2385fcad8e8a498b5723c0098a113ca17cf9
simanthika <juhi37@gmail.com>
2015-11-22 14:02:01 +0800

    Temp File generation code

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
-//Assignments = new Mongo.Collection("assignments");
+// Assignments = new Mongo.Collection("assignments");
 
 if (Meteor.isServer) {
+
+  var fs = Npm.require('fs');
 
 	Meteor.publish('getAssignment', function(){
 		return Assignments.find({});
 	});
+
 
     Meteor.methods({
       saveAssignment: function(id, data) {
@@ -12,9 +15,20 @@ if (Meteor.isServer) {
         	_id: id,
           	data: data
         });
+        createFile(id);
       },
       editAssignment: function(id, data) {
       	Assignments.update({_id: id}, {data: data});
       }
     });
+
+
+
+    fs.writeFile('C:/Users/Gaurav/backend/test.txt', write, function(err){
+      if(err)
+        throw err;
+      console.log("Done");
+    }
+
+
 }
